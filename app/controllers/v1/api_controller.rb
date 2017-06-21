48d5a8632799fc1dd9d2b42d209959133dff5b06
Pham Van Chien <phamvanbk11@gmail.com>
2017-06-21 20:48:01 +0700

    response json

--- app/controllers/v1/api_controller.rb
+++ app/controllers/v1/api_controller.rb
@@ -1,4 +1,10 @@
 class V1::ApiController < ApplicationController
   protect_from_forgery with: :null_session
   respond_to :json
+
+  JsonResponse::STATUS_CODE.keys.each do |status|
+    define_method "response_#{status}" do |message = "", content = {}|
+      render json: JsonResponse.send(status, message, content)
+    end
+  end
 end
--- app/controllers/v1/certifications_controller.rb
+++ app/controllers/v1/certifications_controller.rb
@@ -1,5 +1,6 @@
 class V1::CertificationsController < V1::ApiController
   def index
-    respond_with Certification.all
+    # respond_with Certification.all
+    response_success nil, Certification.all
   end
 end
